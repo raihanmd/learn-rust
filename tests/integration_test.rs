2903ce92ba7b3ca8bb1ab521bafc9fa32a9a8b25
raihanmd <mraihanf471@gmail.com>
2025-08-08 17:34:54 +0700

    add test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -20,6 +20,7 @@ fn test_add_function() {
 	// Suppose your library has an `add` function
 	assert_eq!(add(2, 3), 5);
 	assert_eq!(add(3, 3), 6);
+	assert_eq!(add(3, 4), 7);
 }
 
 // cargo test --test integration_test <FILE_NAME>
