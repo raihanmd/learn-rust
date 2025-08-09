2bd05c2b97021b0c63fd6fc3c1b8ee390d3349c5
raihanmd <mraihanf471@gmail.com>
2025-08-09 16:54:31 +0700

    closure

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,8 @@
 // Integration tests are placed in the `tests` directory at the root of your project.
 // Each file in `tests/` is a separate test crate. You can use your library crate by importing it.
 
+use std::collections::HashMap;
+
 // Example assuming your library crate is named `belajar`:
 use belajar::*; // Replace with your actual crate name
 
@@ -24,3 +26,37 @@ fn test_add_function() {
 }
 
 // cargo test --test integration_test <FILE_NAME>
+
+#[allow(dead_code)]
+struct Cacher<T>
+where
+	T: Fn(i32) -> i32,
+{
+	calculation: T,
+	value: Option<HashMap<i32, i32>>,
+}
+
+impl<T> Cacher<T>
+where
+	T: Fn(i32) -> i32,
+{
+	#[allow(dead_code)]
+	fn new(calculation: T) -> Cacher<T> {
+		Cacher {
+			calculation,
+			value: Some(HashMap::new()),
+		}
+	}
+
+	#[allow(dead_code)]
+	fn value(&mut self, arg: i32) -> i32 {
+		let map = self.value.as_mut().unwrap();
+		if let Some(v) = map.get(&arg) {
+			*v
+		} else {
+			let v = (self.calculation)(arg);
+			map.insert(arg, v);
+			v
+		}
+	}
+}
