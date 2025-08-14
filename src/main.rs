3ff963a875440c24fb49aded9eb3f690dba3e58b
raihanmd <mraihanf471@gmail.com>
2025-08-14 21:17:52 +0700

    learn closures types, FnOnce must be consider, can be confusing

--- src/main.rs
+++ src/main.rs
@@ -1345,3 +1345,71 @@ fn feature() {
 }
 
 // run test based on module, first name of func, etc
+
+#[test]
+fn docs_closure() {
+	#[derive(Debug, PartialEq, Clone, Copy)]
+	enum ShirtColor {
+		Red,
+		Blue,
+	}
+
+	struct Inventory {
+		shirts: Vec<ShirtColor>,
+	}
+
+	impl Inventory {
+		fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+			user_preference.unwrap_or_else(|| self.most_stocked())
+			// .unwrap_or_else(Vec::new) //! will generate []
+		}
+
+		fn most_stocked(&self) -> ShirtColor {
+			let mut red: i32 = 0;
+			let mut blue: i32 = 0;
+
+			for shirt in &self.shirts {
+				match shirt {
+					ShirtColor::Red => red += 1,
+					ShirtColor::Blue => blue += 1,
+				}
+			}
+
+			if red > blue {
+				ShirtColor::Red
+			} else {
+				ShirtColor::Blue
+			}
+		}
+	}
+
+	let store = Inventory {
+		shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+	};
+
+	let user_pref1 = Some(ShirtColor::Red);
+	let giveaway1 = store.giveaway(user_pref1);
+	println!(
+		"The user with preference {:?} gets {:?}",
+		user_pref1, giveaway1
+	);
+
+	let user_pref2 = None;
+	let giveaway2 = store.giveaway(user_pref2);
+	println!(
+		"The user with preference {:?} gets {:?}",
+		user_pref2, giveaway2
+	);
+
+	let mut s = String::from("hello");
+
+	let mut move_closure = || {
+		s.push_str(", world");
+		println!("{}", s);
+		// s //! if only, FnOnce
+	};
+
+	// println!("{}", s); //! Error
+
+	move_closure();
+}
